cbe2f228da6002b4558b957fc919ef2aae89a836
η (eta) <hi@theta.eu.org>
2019-01-21 15:41:02 +0000

    accelerometer test with sliders

--- EngineeringEPQ/App.js
+++ EngineeringEPQ/App.js
@@ -7,24 +7,50 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, Button, Alert, StyleSheet, Text, ScrollView, View, Slider} from 'react-native';
+import {
+	  accelerometer,
+	  gyroscope,
+	  setUpdateIntervalForType,
+	  SensorTypes
+} from "react-native-sensors";
+import { map, filter } from "rxjs/operators";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+	  super(props);
+	  this.state = {"accel": {}};
+	  this.state.accel = {"x": 0, "y": 0, "z": 0};
+	  var self = this;
+	  setUpdateIntervalForType(SensorTypes.accelerometer, 100);
+	  const subscription = accelerometer
+		  .subscribe(({x, y, z, timestamp}) => {
+			  self.setState(prev => {
+				  prev.accel.x = x;
+				  prev.accel.y = y;
+				  prev.accel.z = z;
+				  return prev;
+			  })
+		  });
+  }
   render() {
     return (
-      <View style={styles.container}>
-	<Text style={styles.welcome}>Ooh look, maybe I can convince myself my EPQ isn't a failure</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <ScrollView style={styles.container}>
+
+	    <Button
+	    onPress={() => Alert.alert('Yay!')}
+	    title="Button!"
+	    />
+	<Text style={styles.welcome}>Ooh look, maybe I can convince myself my EPQ isn't a failure!</Text>
+        <Text style={styles.instructions}>x: {this.state.accel.x.toPrecision(3)}</Text>
+        <Text style={styles.instructions}>y: {this.state.accel.y.toPrecision(3)}</Text>
+        <Text style={styles.instructions}>z: {this.state.accel.z.toPrecision(3)}</Text>
+	    <Slider style={styles.slider} maximumValue={10} minimumValue={-10} value={this.state.accel.x} />
+	    <Slider style={styles.slider} maximumValue={10} minimumValue={-10} value={this.state.accel.y} />
+	    <Slider style={styles.slider} maximumValue={8} minimumValue={13} value={this.state.accel.z} />
+      </ScrollView>
     );
   }
 }
@@ -32,9 +58,10 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  slider: {
+	  width: 500
   },
   welcome: {
     fontSize: 20,
