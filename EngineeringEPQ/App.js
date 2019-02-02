b4899f263b5780f6b04f5c54980d03812179aa9d
η (eta) <hi@theta.eu.org>
2019-02-02 18:30:46 +0000

    some form of smoothing?

--- EngineeringEPQ/App.js
+++ EngineeringEPQ/App.js
@@ -18,7 +18,13 @@ import { LineChart } from 'react-native-svg-charts'
 import { map, filter } from "rxjs/operators";
 import LPF from 'lpf';
 
-
+function smooth(smoothed, smoothing, lastUpdate, newval) {
+	var now = new Date;
+	var elapsedTime = now - lastUpdate;
+	smoothed += elapsedTime * ( newval - smoothed ) / smoothing;
+	lastUpdate = now;
+	return smoothed;
+}
 type Props = {};
 export default class App extends Component<Props> {
   constructor(props) {
@@ -27,6 +33,9 @@ export default class App extends Component<Props> {
 	  LPF.init([]);
 	  this.state = {"accel": {}, "zdata": [], "zdata_lpf": [], "lpf": LPF};
 	  this.state.accel = {"x": 0, "y": 0, "z": 0};
+	  this.state.smoothed   = 0;        // or some likely initial value
+	  this.state.smoothing  = 10;       // or whatever is desired
+	  this.state.lastUpdate = new Date;
 	  var self = this;
 	  setUpdateIntervalForType(SensorTypes.accelerometer, 10);
 	  const subscription = accelerometer
@@ -35,13 +44,9 @@ export default class App extends Component<Props> {
 				  prev.accel.x = x;
 				  prev.accel.y = y;
 				  prev.accel.z = z;
-				  prev.zdata.push(z);
+				  prev.zdata.push(smooth(prev.smoothed, prev.smoothing, prev.lastUpdate, z));
 				  if (prev.zdata.length > 40) {
 					  prev.zdata.shift();
-				  }
-				  prev.zdata_lpf.push(prev.lpf.next(z));
-				  if (prev.zdata_lpf.length > 40) {
-					  prev.zdata_lpf.shift();
 				  }
 				  return prev;
 			  })
@@ -58,22 +63,13 @@ export default class App extends Component<Props> {
 	    <Slider style={styles.slider} maximumValue={10} minimumValue={-10} value={this.state.accel.x} />
 	    <Slider style={styles.slider} maximumValue={10} minimumValue={-10} value={this.state.accel.y} />
 	    <Slider style={styles.slider} maximumValue={8} minimumValue={13} value={this.state.accel.z} />
-	    <Text style={styles.instructions}>lpf smoothing: {this.state.lpf.smoothing}</Text>
-	    <Button onPress={function () { self.state.zdata = []; self.state.lpf.buffer = []; }} title="Clear data" />
-	    <Slider style={styles.slider} maximumValue={0.95} minimumValue={0} value={this.state.lpf.smoothing} onValueChange={function (val) { self.state.lpf.smoothing = val; }} />
+	    <Text style={styles.instructions}>lpf smoothing: {this.state.smoothing}</Text>
+	    <Slider style={styles.slider} maximumValue={100} minimumValue={1} value={this.state.smoothing} onValueChange={function (val) { self.state.smoothing = val; }} />
 	    <LineChart
 	    style={ { height: 200, width: "100%" } }
 	    data={ this.state.zdata }
 	    svg={ {
 		    stroke: 'rgb(134, 65, 244)',
-	    } }
-	    >
-	    </LineChart>
-	    <LineChart
-	    style={ { height: 200, width: "100%" } }
-	    data={ this.state.zdata_lpf }
-	    svg={ {
-		    stroke: 'rgb(65, 65, 244)',
 	    } }
 	    >
 	    </LineChart>
