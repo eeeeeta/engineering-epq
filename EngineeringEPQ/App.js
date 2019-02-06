69c3c16347292d218aeb648c21a255e3097312a1
eeeeeta <eeeeeta@users.noreply.github.com>
2019-02-06 21:01:53 +0000

    implement FootPath algorithm in JS, make a nice testing rig

--- EngineeringEPQ/App.js
+++ EngineeringEPQ/App.js
@@ -1,13 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, Button, Alert, StyleSheet, Text, ScrollView, View, Slider} from 'react-native';
+import {Platform, Button, Alert, StyleSheet, Text, ScrollView, View, Slider, Switch} from 'react-native';
 import {
   accelerometer,
   gyroscope,
@@ -17,15 +9,8 @@ import {
 import { LineChart } from 'react-native-svg-charts'
 import { map, filter } from "rxjs/operators";
 import LPF from 'lpf';
-import { StepDetector } from './lib/steps';
+import { StepDetector, BestFitAlgorithm } from './lib/steps';
 
-function smooth(smoothed, smoothing, lastUpdate, newval) {
-  var now = new Date;
-  var elapsedTime = now - lastUpdate;
-  smoothed += elapsedTime * ( newval - smoothed ) / smoothing;
-  lastUpdate = now;
-  return smoothed;
-}
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -33,10 +18,25 @@ export default class App extends Component {
     this.state = {"steps": [], "heading": 0};
     var steps = this.state.steps;
     this.state.step_detector = new StepDetector();
+    this.state.alg = new BestFitAlgorithm();
+    this.state.alg_steps = [];
+    this.state.recording = false;
+    this.state.detecting = false;
     this.state.step_detector.on_drop_found = function(diff, hdg) {
       self.setState((prev) => {
-	prev.heading = hdg;
-	prev.steps.push(hdg + "° " + diff.toFixed(2) + " m/s^2 @ " + Date.now());
+	if (prev.detecting) {
+	  prev.heading = hdg;
+	  prev.steps.push(hdg + "° " + diff.toFixed(2) + " m/s^2 @ " + Date.now());
+	  if (prev.recording) {
+	    prev.alg.route_headings.push(hdg);
+	  }
+	  else {
+	    prev.alg.detected_headings.push(hdg);
+	    console.log(prev.alg.detected_headings);
+	    console.log(prev.alg.route_headings);
+	    console.log(prev.alg.matrix);
+	  }
+	}
 	return prev;
       });
     };
@@ -44,20 +44,55 @@ export default class App extends Component {
   render() {
     var self = this;
     const steps = self.state.steps.map((step, idx) => {
-      console.log("step!");
       return (<Text key={idx} style={styles.instructions}>{step}</Text>);
     });
+    const switchState = (val) => {
+      self.setState((prev) => {
+	prev.recording = val;
+	return prev;
+      });
+    };
+    const switchDetecting = (val) => {
+      self.setState((prev) => {
+	prev.detecting = val;
+	return prev;
+      });
+    };
+    var pos = 0;
+    if (!self.state.recording) {
+      self.state.alg.recalc_matrix();
+      pos = self.state.alg.get_pos() || 0;
+    }
+    const clearStepsRec = () => {
+      self.setState((prev) => {
+	prev.steps = [];
+	prev.alg.route_headings = [];
+	return prev;
+      });
+    };
     const clearSteps = () => {
       self.setState((prev) => {
 	prev.steps = [];
+	prev.alg.detected_headings = [];
 	return prev;
       });
     };
     return (
       <ScrollView style={styles.container}>
-      <Text style={styles.welcome}>EPQ Accelerometer Testing</Text>
-      <Text style={styles.welcome}>Heading: {self.state.heading}°</Text>
-      <Button title="Clear steps" onPress={clearSteps} />
+      <Text style={styles.welcome}>EPQ FootPath testing</Text>
+      <Text style={styles.status}>Heading: {self.state.heading}°</Text>
+      <View style={{flex: 1, flexDirection: 'row', alignItems: 'stretch'}}>
+      <Text style={styles.status}>Detect steps? ({self.state.alg.detected_headings.length} steps)</Text>
+      <Switch value={self.state.detecting} onValueChange={switchDetecting} />
+      </View>
+      <View style={{flex: 1, flexDirection: 'row', alignItems: 'stretch'}}>
+      <Text style={styles.status}>Recording steps? ({self.state.alg.route_headings.length} steps)</Text>
+      <Switch value={self.state.recording} onValueChange={switchState} />
+      </View>
+      <Text style={styles.status}>Pos: {pos} of {self.state.alg.route_headings.length-1}</Text>
+      <Slider style={styles.slider} maximumValue={self.state.alg.route_headings.length-1} minimumValue={0} value={pos} />
+      <Button title="Clear detected steps" onPress={clearSteps} />
+      <Button title="Clear recorded steps" onPress={clearStepsRec} />
 
       <Text style={styles.instructions}>difference_accel: {self.state.step_detector.difference_accel} m/s^2</Text>
       <Slider
@@ -103,12 +138,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   slider: {
-    width: 500
+    width: 400
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+  },
+  status: {
+    fontSize: 18,
+    margin: 5
   },
   instructions: {
     textAlign: 'center',
--- EngineeringEPQ/lib/steps.js
+++ EngineeringEPQ/lib/steps.js
@@ -9,7 +9,92 @@ import {
 } from "react-native-sensors";
 import _ from "lodash";
 import LPF from 'lpf';
+class BestFitAlgorithm {
+  constructor() {
+    var self = this;
 
+    self.route_headings = [];
+    self.detected_headings = [];
+    self.matrix = [];
+  }
+  score(alpha, beta) {
+    let diff = 180.0 - Math.abs(Math.abs(alpha - beta) - 180.0);
+    if (diff <= 45.0) {
+      return 0.0;
+    }
+    else if (diff <= 90.0) {
+      return 1.0;
+    }
+    else if (diff <= 120.0) {
+      return 2.0;
+    }
+    else {
+      return 10.0;
+    }
+  }
+  get_score(m, s) {
+    var self = this;
+    if (m == 0 || s == 0) {
+      return Infinity;
+    }
+    else {
+      return self.score(self.route_headings[m-1], self.detected_headings[s-1]);
+    }
+  }
+  get_d(i, j) {
+    if (i == 0 && j == 0) {
+      return 0.0;
+    }
+    else if (i == 0 || j == 0) {
+      return Infinity;
+    }
+    else {
+      return this.matrix[i-1][j-1];
+    }
+  }
+  set_d(i, j, val) {
+    this.matrix[i-1][j-1] = val;
+  }
+  recalc_matrix() {
+    var self = this;
+    let ret = [];
+    for (let i = 0; i < self.route_headings.length; i++) {
+      let ivec = [];
+      for (let j = 0; j < self.detected_headings.length; j++) {
+	ivec.push(Infinity);
+      }
+      ret.push(ivec);
+    }
+    self.matrix = ret;
+    console.log(self.matrix);
+    for (let i = 0; i < self.route_headings.length; i++) {
+      let eye = i + 1;
+      for (let j = 0; j < self.detected_headings.length; j++) {
+	let jay = j + 1;
+	let first = self.get_d(eye - 1, jay - 1) + self.get_score(eye, jay);
+	let second = self.get_d(eye - 1, jay) + self.get_score(eye, jay - 1) + 1.5;
+	let third = self.get_d(eye, jay - 1) + self.get_score(eye - 1, jay) + 1.5;
+	let result = Math.min(first, second, third);
+	self.set_d(eye, jay, result);
+      }
+    }
+  }
+  get_pos() {
+    var self = this;
+    let curmin;
+    let val = Infinity;
+    let j = self.detected_headings.length;
+    for (let i = 0; i < self.route_headings.length; i++) {
+      let eye = i + 1;
+      let newval = self.get_d(eye, j);
+      if (newval < val) {
+	curmin = i;
+	val = newval;
+      }
+    }
+    return curmin;
+  }
+}
 class StepDetector {
   constructor() {
     var self = this;
@@ -23,8 +108,8 @@ class StepDetector {
     // Data storage
     self.lpf = LPF;
     self.lpf.init([]);
-    self.lpf.smoothing = 0.6;
-    self.lpf.bufferMaxSize = 20;
+    self.lpf.smoothing = 0.7;
+    //self.lpf.bufferMaxSize = 20;
     self.readings = [];
     self.readings_length = 100;
     self.on_drop_found = function() {};
@@ -53,13 +138,13 @@ class StepDetector {
   on_data(x, y, z, timestamp) {
     var self = this;
     let smoothed = LPF.next(z);
-    console.log("on_data:", z, smoothed, timestamp);
+    //console.log("on_data:", z, smoothed, timestamp);
     self.readings.push([smoothed, timestamp]);
     if (self.readings.length == self.readings_length) {
       self.readings.shift();
     }
     if ((self.last_detection_time + self.timeout_ms) >= timestamp) {
-      console.log("within window");
+      //console.log("within window");
       return;
     }
     // Look at readings within the window only
@@ -67,7 +152,7 @@ class StepDetector {
       return (ts + self.timeout_ms) >= timestamp;
     });
     if (start_idx) {
-      console.log("found start idx", start_idx, self.readings.length);
+      //console.log("found start idx", start_idx, self.readings.length);
       // find the highest accel value
       let highest_accel = 0.0;
       let highest_accel_idx = undefined;
@@ -77,7 +162,7 @@ class StepDetector {
 	  highest_accel = self.readings[idx][0];
 	}
       }
-      console.log("highest accel", highest_accel);
+      //console.log("highest accel", highest_accel);
       // find any drops with difference_accel in this window
       let drop_found = false;
       let drop_diff = undefined;
@@ -96,8 +181,8 @@ class StepDetector {
       }
     }
     else {
-      console.log("no start idx");
+      //console.log("no start idx");
     }
   }
 }
-export { StepDetector };
+export { StepDetector, BestFitAlgorithm };
